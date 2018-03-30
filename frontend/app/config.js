7ea2705b8f8bbf11fd3c0f99d78c303a707b0769
devanshpatel <dev@cloudcade.com>
2018-03-30 01:59:55 -0700

    change

--- frontend/app/config.js
+++ frontend/app/config.js
@@ -1,5 +1,5 @@
 // if (process.env.NODE_ENV === 'production') {
-exports.API_URL = 'https://minicoinlist.herokuapp.com//v1';
+exports.API_URL = 'https://minicoinlist.herokuapp.com/v1';
 // }
 // else {
 //     exports.API_URL = "http://localhost:3000/v1"
